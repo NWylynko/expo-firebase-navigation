830277af881bc293c2aad7bcd78f3cfec7543f0e
Nick Wylynko <nick1014375@gmail.com>
2020-03-14 19:10:16 +0800

    dont need that stuff

--- firebase/index.js
+++ firebase/index.js
@@ -1,15 +1,9 @@
 import firebaseConfig from './config';
-import app from "firebase/app";
 
-import "firebase/auth";
+import app from "firebase/app";
 import "firebase/database";
 
 app.initializeApp(firebaseConfig);
-
-// Authentication api
-
-export const doSignOut = () =>
-  app.auth().signOut();
 
 export default app;
 
